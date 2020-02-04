b522f24063a411d287e74cd0c70ecee4a1ad5b56
ktr11 <kenmcz315@gmail.com>
2020-02-04 23:52:27 +0900

    ・第五章まで ・rubocopの導入

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,17 +1,20 @@
-require "test_helper"
+require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test "layout links" do
+  test 'layout links' do
     # rootにリクエスト
     get root_path
     # homeのテンプレートが読み込まれる
-    assert_template "static_pages/home"
+    assert_template 'static_pages/home'
     # 各リンクの有無を確認
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', signup_path
     get contact_path
-    assert_select "title", full_title("Contact")
+    assert_select 'title', full_title('Contact')
+    get signup_path
+    assert_select 'title', full_title('Sign up')
   end
 end
