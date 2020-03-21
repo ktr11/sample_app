c8a038a3108e020a142264d4aa0271e0071c3204
ktr11 <kenmcz315@gmail.com>
2020-03-21 23:55:34 +0900

    リスト6.2.4まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # userオブジェクトの各フィールドの形式等を定義
 class User < ApplicationRecord
   validates(:name, presence: true, length: { maximum: 50 })
-  VALID_EMAIL_REGEX = /\A[a-z\d]+[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_EMAIL_REGEX =
+    /\A[a-z\d]+[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates(:email, presence: true, length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX })
-
 end
