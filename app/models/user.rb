42f2fc928bd906c74d0137df431d26dd75a6bc25
ktr11 <kenmcz315@gmail.com>
2020-03-22 01:00:08 +0900

    6.2が終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 # userオブジェクトの各フィールドの形式等を定義
 class User < ApplicationRecord
+  # 保存前にメールアドレスを小文字に
+  before_save { email.downcase! }
   validates(:name, presence: true, length: { maximum: 50 })
   VALID_EMAIL_REGEX =
     /\A[a-z\d]+[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates(:email, presence: true, length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX })
+    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false })
 end
