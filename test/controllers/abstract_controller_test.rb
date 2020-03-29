e6d3e27b56d9c8fc2538231672c89601c809e86c
ktr11 <kenmcz315@gmail.com>
2020-03-30 00:19:48 +0900

    チュートリアルにはないが、テストを書いてみた。

---
+++ test/controllers/abstract_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AbstractControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = 'Ruby on Rails Tutorial Sample App'
+  end
+end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,37 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < AbstractControllerTest
+  def setup
+    super
+    @user = User.new(name: 'Example User', email: 'user@example.com',
+      password: 'foobar', password_confirmation: 'foobar')
+    @user.save
+  end
+
   test 'should get new' do
     get signup_path
     assert_response :success
+  end
+
+  test 'should get new 2' do
+    get '/users/new'
+    assert_response :success
+    assert_select("input[type='text'][name='user[name]'][id='user_name']", count: 1)
+    assert_select("input[type='email'][name='user[email]'][id='user_email']", count: 1)
+    assert_select("input[type='password'][name='user[password]'][id='user_password']", count: 1)
+    assert_select("input[type='password'][name='user[password_confirmation]'][id='user_password_confirmation']",
+                  count: 1)
+    assert_select("input[type='submit'][name='commit']", count: 1)
+    assert_select('form.new_user input', count: 5)
+  end
+
+  test 'should get user' do
+    id = @user.id
+    get "/users/#{id}"
+    assert_response :success
+    assert_select('title', "#{@user.name} | #{@base_title}")
+    gravatar_url = 'https://secure.gravatar.com/avatar/'
+    assert_select("img[alt='#{@user.name}'][src*='#{gravatar_url}']")
+    assert_select('h1', /#{@user.name}/)
   end
 end
