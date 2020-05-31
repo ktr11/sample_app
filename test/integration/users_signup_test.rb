01c75bc1664a5b83fb1ad47029256d4bdaa2ddd7
ktr11 <kenmcz315@gmail.com>
2020-05-31 23:47:09 +0900

    7.5 の上まで

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,4 +16,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       assert_select 'div.alert-danger'
     end
   end
+
+  test 'valid signup infomation' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'Example User',
+                                         email: 'user@example.com',
+                                         password: 'password',
+                                         password_confirmation: 'password' } }
+    end
+    follow_redirect! # 正しいリダイレクト先に遷移するよね？というメソッド
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
