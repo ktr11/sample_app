826966988cfcd68b99d28dc3b732054893bc1d03
ktr11 <kenmcz315@gmail.com>
2020-06-14 12:46:19 +0900

    rails 6 の Webpackを使用した導入方法でリスト8.20にあたる導入を実行

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+equire("jquery")
+import 'bootstrap'
+import '../src/application.scss'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
