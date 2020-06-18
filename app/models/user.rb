3278a289f84d42da03b95c996283b9e5ebaef659
ktr11 <kenmcz315@gmail.com>
2020-06-19 00:23:59 +0900

    9.1.2の上まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 # userオブジェクトの各フィールドの形式等を定義
 class User < ApplicationRecord
+  attr_accessor :remember_token
   # 保存前にメールアドレスを小文字に
-  before_save { email.downcase! }
+  before_save { self.email = email.downcase }
   # name のvalidation
   validates(:name, presence: true, length: { maximum: 50 })
   # email の validation
@@ -17,5 +18,15 @@ class User < ApplicationRecord
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
