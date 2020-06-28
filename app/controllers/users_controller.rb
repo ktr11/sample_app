7155e262ba4b7a727f2a9726e6953c72d080f792
ktr11 <kenmcz315@gmail.com>
2020-06-28 22:36:17 +0900

    10.3.1 の上まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 # User Resource Controller
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[edit update]
+  before_action :correct_user,   only: %i[edit update]
+  # Userページ表示
   def show
     @user = User.find(params[:id])
   end
 
+  # User新規作成 初期表示
   def new
     @user = User.new
   end
 
+  # User作成
   def create
     @user = User.new(user_params)
     if @user.save
@@ -19,12 +24,12 @@ class UsersController < ApplicationController
     end
   end
 
+  # User編集 初期表示
   def edit
-    @user = User.find(params[:id])
   end
 
+  # User更新
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'Profile updated'
       redirect_to @user
@@ -37,5 +42,22 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # beforeアクション
+
+    # ログイン済ユーザーかどうか確認
+    def logged_in_user
+      return if logged_in?
+
+      store_location
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
