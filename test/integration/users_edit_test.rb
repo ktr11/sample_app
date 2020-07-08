b58d05b0834eeab15983b979bf8266a870b61f96
ktr11 <kenmcz315@gmail.com>
2020-07-08 22:44:21 +0900

    テストのファイル構成を変更 controllerのみの検証をintegrationからcontrollersのほうに移動

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -36,34 +36,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_not @user.password_digest.empty?
   end
 
-  test 'should redirect edit when not logged in' do
-    get edit_user_path(@user)
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
-  test 'should redirect update when not logged in' do
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
-  test 'should redirect edit when logged in as wrong user' do
-    t_log_in_as(@other_user)
-    get edit_user_path(@user)
-    assert flash.empty?
-    assert_redirected_to root_url
-  end
-
-  test 'should redirect update when logged in as wrong user' do
-    t_log_in_as(@other_user)
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
-    assert flash.empty?
-    assert_redirected_to root_url
-  end
-
   test 'successful edit with friendly forwarding' do
     get edit_user_path(@user)
     assert_equal session[:forwarding_url], edit_user_url(@user)
