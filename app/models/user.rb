a9b06cd8f5e8e054b03eb8e91969686129185e63
ktr11 <kenmcz315@gmail.com>
2020-07-16 01:45:07 +0900

    11.1.2 おわり

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # userオブジェクトの各フィールドの形式等を定義
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  # 保存前にメールアドレスを小文字に
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   # name のvalidation
   validates(:name, presence: true, length: { maximum: 50 })
   # email の validation
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     /\A[a-z\d]+[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates(:email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false })
-  # セキュアなパスワード
+  # passwordのvalidation セキュアなパスワード
   has_secure_password
   validates(:password, presence: true, length: { minimum: 6 }, allow_nil: true)
 
@@ -42,4 +42,17 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+
+    # メールアドレスを小文字に変更
+    def downcase_email
+      email.downcase!
+    end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
