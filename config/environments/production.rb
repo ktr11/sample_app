ecb66bff55ccf878f516c36245b1f98f567e22c3
ktr11 <kenmcz315@gmail.com>
2020-07-24 15:42:47 +0900

    アプリ変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'rails-tutorial-sampleapp-ktr11.herokuapp.com'
+  host = 'rails-sample-ktr11.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
