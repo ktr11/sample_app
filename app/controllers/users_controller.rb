11e1af1fe7f88419e3e6c81609fe3ae930c6bf5f
ktr11 <kenmcz315@gmail.com>
2020-07-24 14:43:46 +0900

    11.3.3 まで終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,13 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   # Userページ表示
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   # User新規作成 初期表示
@@ -22,9 +23,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in(@user)
-      flash[:success] = 'Welcome to the Sample App!'
-      redirect_to user_url(@user)
+      @user.send_activation_email
+      flash[:info] = 'Please check your email to activate your account.'
+      redirect_to root_url
     else
       render 'new'
     end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test 'invalid signup information' do
     get signup_path
     assert_select 'form[action="/users"]'
@@ -17,7 +21,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test 'valid signup infomation' do
+  test 'valid signup infomation with account activation' do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: 'Example User',
@@ -25,6 +29,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password: 'password',
                                          password_confirmation: 'password' } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    # 有効化していない状態でログインしてみる
+    t_log_in_as(user)
+    assert_not t_logged_in?
+    # 有効化トークンが不正な場合
+    get edit_account_activation_path('invalid token', email: user.email)
+    assert_not t_logged_in?
+    # トークンは正しいがメールアドレスが無効な場合
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not t_logged_in?
+    # 有効化トークンが正しい場合
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect! # テスト対象をリダイレクト先に移すメソッド
     assert_template 'users/show'
     assert_not flash.empty?
