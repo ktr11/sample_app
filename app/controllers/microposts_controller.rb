5446e94c4c8008b056b80e32839a98b2ab1231ca
ktr11 <kenmcz315@gmail.com>
2020-08-20 22:44:55 +0900

    13.3.5 まで

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -21,8 +21,7 @@ class MicropostsController < ApplicationController
   def destroy
     @micropost.destroy
     flash[:success] = 'Micropost deleted'
-    # redirect_back(fallback_location: root_url)
-    redirect_to request.referrer || root_url
+    redirect_back(fallback_location: root_url)
   end
 
   private
