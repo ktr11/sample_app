1cbc9d833739b44f30db97e2458615da09a6f364
ktr11 <kenmcz315@gmail.com>
2020-08-25 00:03:20 +0900

    13.4.2の上まで

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,7 @@
 # micropostオブジェクトの各フィールドの形式などを定義
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
